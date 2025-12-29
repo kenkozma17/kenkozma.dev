0202868ceb2ae873883a328750af3e42a2210680
kenkozma17 <kenkozma17@gmail.com>
2025-12-29 20:31:46 +0800

    Added nodejs compat

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1767010989134,
+    __BUILD_TIMESTAMP_MS__: 1767011427011,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
